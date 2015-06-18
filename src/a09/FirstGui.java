5d5039ae6158a65bed4ab28761fd49453781566f
Christoph Stumpe <ch.stumpe@web.de>
2015-06-18 13:23:46 +0200

    aunfgabe 9 fertig

---
+++ src/a09/FirstGui.java
@@ -0,0 +1,39 @@
+package a09;
+
+import a08.SmileyModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class FirstGui {
+    public static void main(String[] args) {
+        int radius=500;
+        SmileyModel test = new SmileyModel(radius, true);
+        FirstSmileyPanel view = new FirstSmileyPanel(test);
+        JFrame frame = new JFrame("Smiley of DOOM!!!");
+        Container container = frame.getContentPane();
+        container.add(view);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
+        frame.setVisible(true);
+        int i=0;
+        while(true){
+            test.rotateEye(i);
+            i+=10;
+            if(i%90==0){
+                if(test.isSmile()){
+                    test.setSmile(false);
+                    continue;
+                }
+                test.setSmile(true);
+                //test.setSize(radius+=20);
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
